54bfa29b936155c03b1306131c9723c24e274dd9
Thais de Campos <thaisd.campos@gmail.com>
2024-12-20 11:05:50 -0300

    refactory de forward method

--- move_rover.rb
+++ move_rover.rb
@@ -43,14 +43,29 @@ class MoveRover
     @position[:cardinal_point] = DIRECTIONS[(DIRECTIONS.index(@position[:cardinal_point]) + step) % 4]
   end
 
-  def move_foward
+  def move_forward
     move = MOVES[@position[:cardinal_point]]
+
+    return if invalid_destination?(move)
 
     @position[:x] += move[:x]
     @position[:y] += move[:y]
   end
 
-  def invalid_position?
-    @position[:x] >= @size[:x] && @position[:y] >= @size[:y]
+  def invalid_destination?(move)
+    x_axis = @position[:x] + move[:x]
+    y_axis = @position[:y] + move[:y]
+
+    out_of_bounds?(x_axis, y_axis)
+  end
+
+  def out_of_bounds?(x_axis, y_axis)
+    x_axis.negative? || y_axis.negative? || x_axis > @size[:x] || y_axis > @size[:y]
+  end
+
+  def finish_position(positions)
+    [positions].map do |position|
+      "#{position[:x]} #{position[:y]} #{position[:cardinal_point]}"
+    end
   end
 end
